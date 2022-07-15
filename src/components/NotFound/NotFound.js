23fe7eb6dbf659945084024053fb4b25769bd58d
ZakirH88 <cmzakirh77@gmail.com>
2022-07-16 02:57:22 +0600

    update commit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import img from './image/banner_error_404.jpg'; 
 
 const NotFound = () => {
 	return (
 		<div>
-			<h1>The Page Not Found</h1>
+			<img src={img} alt=" " />
+			{/* <h1>The Page Not Found</h1> */}
 		</div>
 	);
 };
